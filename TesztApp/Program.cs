6b982a2601c2d585804255e4899af42e4aa44eac
Tamás Iványi <terraled54@gmail.com>
2020-11-21 13:49:51 +0100

    Add project files.

---
+++ TesztApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesztApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
